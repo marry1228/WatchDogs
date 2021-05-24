44f2cd6e0872902c94ebf00c48ec9241586d830e
marry1228 <jinyoungkimdotcom@gmail.com>
2021-05-24 10:20:57 +0900

    2021-05-24

--- WatchDogs_jy/src/main/java/com/watchdogs/command/base/BaseCommand.java
+++
@@ -1,9 +0,0 @@
-package com.watchdogs.command.base;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface BaseCommand { // 메소드 이름만 정의 ; 회사마다 포맷이 틀리다. 처음에 팀별로 잘 정하기
-
-	public void execute(HttpServletRequest request, HttpServletResponse response);
-}
