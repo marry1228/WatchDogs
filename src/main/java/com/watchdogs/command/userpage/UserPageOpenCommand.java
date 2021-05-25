959c7fd2b6ab6c59d30b8d671f41b5fe2c368b6a
marry1228 <jinyoungkimdotcom@gmail.com>
2021-05-26 01:29:20 +0900

    2021-05-25

---
+++ src/main/java/com/watchdogs/command/userpage/UserPageOpenCommand.java
@@ -0,0 +1,16 @@
+package com.watchdogs.command.userpage;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.watchdogs.command.home.BCommand;
+
+public class UserPageOpenCommand implements BCommand {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
